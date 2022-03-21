21018d6352626c9ea0373c14ec7d9fc72d3d2106
micronyks <shah.nikhilkumar@ltts.com>
2022-03-22 01:28:54 +0530

    updated action

--- packages/webapp/src/components/error-boundary.component.tsx
+++ packages/webapp/src/components/error-boundary.component.tsx
@@ -25,7 +25,7 @@ class ErrorBoundary extends Component<Props, State> {
 
     public render() {
         if (this.state.hasError) {
-            return <h1>Something went wrong ! Team is working for better user expeirnce.</h1>;
+            return <h1>Something went wrong ! Team is working for better user expeirnce...</h1>;
         }
 
         return this.props.children;
