479afdc194a3100dcb98622e898028ca65f22d44
micronyks <nikhilshahlive@gmail.com>
2022-07-28 19:28:56 +0530

    testing

--- packages/header/src/components/header.component.tsx
+++ packages/header/src/components/header.component.tsx
@@ -1,11 +1,14 @@
 import { Fragment } from "react";
 import NavbarComponent from "./navbar.component";
 
+const reactImage = require("../images/logo192.png");
+
 const HeaderComponent: React.FC<{ navigateTo: (routeTo: string) => void, profileMenuClickHandler: (menuItem: string) => void, isAuthenticated: boolean }> = (props) => {
     return <Fragment>
         <NavbarComponent navigateTo={props.navigateTo} profileMenuClickHandler={props.profileMenuClickHandler} isAuthenticated={props.isAuthenticated} />
 
-        <img src={require("../images/logo192.png")}></img>
+        <h1>Testing Header Image</h1>
+        <img src={reactImage}></img>
     </Fragment>
 }
 
